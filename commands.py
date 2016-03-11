d3a650544935d4b1abd90158d4eb7f1613a8d93f
Magnus-RC <magnus.rc.den@gmail.com>
2016-03-11 22:11:55 +0100

    creating commands.py & editing prompt.py

---
+++ commands.py
@@ -0,0 +1,10 @@
+class command(object):
+    listOfCommands = ["help --showing a list of commands", "quit --for quitting the program"]
+
+    def __init__(self, serviceName):
+        self.serviceName = serviceName
+
+    #  def help(self):
+
+
+
--- prompt.py
+++ prompt.py
@@ -1,1 +1,17 @@
 """prompt.py in Pycryption"""
+
+
+def cmd():
+    # mmm
+    crash = False
+    while crash != True:
+        input = str(raw_input(">"))
+        if input == "":
+            print "Remember to write something before hitting ENTER."
+            continue
+        elif input == "quit" or input == "exit":
+            crash = True
+            # bye bye
+        elif input == "help" or input == "?":
+            help()
+
